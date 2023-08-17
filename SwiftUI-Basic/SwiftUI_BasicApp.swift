eba9fa59e85f6808bf2300009b49aace2810de53
최지연 <jiyeonchoi@tmapmobility.com>
2023-08-17 14:19:44 +0900

    Initial Commit

---
+++ SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by 최지연/클라이언트 on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
